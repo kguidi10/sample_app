2efc1c3854838a0be3b29755699b60e59ec2b216
Kristin Guidi <kristinguidi@gmail.com>
2017-09-12 12:32:20 -0700

    5.10 admin and moderating comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 	has_many :orders
 	has_many :comments
 end
+
+
