6a205b4d1508d1e256316214ad56fa9642e034de
Kristin Guidi <kristinguidi@gmail.com>
2017-10-23 18:31:45 -0700

    chgs for stripe error

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,11 +1,11 @@
 if Rails.env.production?
   Rails.configuration.stripe = {
-    publishable_key: 'Spk_test_AqriVAfRZ5Zk0N6yUr1bTVzj',
+    publishable_key: 'pk_test_AqriVAfRZ5Zk0N6yUr1bTVzj',
     secret_key: 'sk_test_wbb84Pbql9eb5TrHs371b3wW'
   }
 else
   Rails.configuration.stripe = {
-    publishable_key: 'Spk_test_AqriVAfRZ5Zk0N6yUr1bTVzj',
+    publishable_key: 'pk_test_AqriVAfRZ5Zk0N6yUr1bTVzj',
     secret_key: 'sk_test_wbb84Pbql9eb5TrHs371b3wW'
   }
 end
