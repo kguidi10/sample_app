4640847648667a0d2905770dc480ce07949937d4
Kristin Guidi <kristinguidi@gmail.com>
2017-10-25 12:16:33 -0700

    chg to price_to_decimal

--- db/migrate/20171024024916_change_price_to_decimal.rb
+++ db/migrate/20171024024916_change_price_to_decimal.rb
@@ -1,5 +1,5 @@
 class ChangePriceToDecimal < ActiveRecord::Migration[5.1]
-  def change
-  	change_column :products, :price, :decimal
+   def change
+    change_column :products, :price, :int
   end
 end
