1859a6403b30ce089e0134079a3411efd02628d3
Kristin Guidi <kristinguidi@gmail.com>
2017-10-25 19:08:38 -0700

    added in db

--- db/migrate/20171024024916_change_price_to_decimal.rb
+++ db/migrate/20171024024916_change_price_to_decimal.rb
@@ -1,5 +1,10 @@
 class ChangePriceToDecimal < ActiveRecord::Migration[5.1]
-   def change
-    change_column :products, :price, :int
-  end
+  	def self.up
+		remove_foreign_key :products, column: :price
+		change_column :products, :price, :decimal
+	end
+	def self.down
+		add_foreign_key :products, :price
+		change_column :products, :price, :string
+	end
 end
