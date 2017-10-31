1597fed59f5cbdd02a2bd18cd86a5367bc593185
Kristin Guidi <kristinguidi@gmail.com>
2017-10-30 18:52:25 -0700

    chg price/int

--- db/migrate/20171024024916_change_price_to_decimal.rb
+++ db/migrate/20171024024916_change_price_to_decimal.rb
@@ -1,5 +1,5 @@
 class ChangePriceToDecimal < ActiveRecord::Migration[5.1]
   	def change
-		change_column :products, :price, :decimal
+		change_column :products, :price, 'integer USING CAST(price AS integer)'
 	end
 end
