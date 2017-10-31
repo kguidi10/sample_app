846b07f3fd2d22918d7abbdcf909cf97e611583d
Kristin Guidi <kristinguidi@gmail.com>
2017-10-30 18:27:13 -0700

    chgs for decimal

--- db/migrate/20171024024916_change_price_to_decimal.rb
+++ db/migrate/20171024024916_change_price_to_decimal.rb
@@ -1,10 +1,5 @@
 class ChangePriceToDecimal < ActiveRecord::Migration[5.1]
-  	def self.up
-		remove_foreign_key :products, column: :price
-		change_column :products, :price, :decimal
-	end
-	def self.down
-		add_foreign_key :products, :price
-		change_column :products, :price, :string
+  	def change
+		change_column :products, :price, :integer
 	end
 end
