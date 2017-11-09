a71b76b6cc2a2e25ba81a1321f1b901151e11b32
Kristin Guidi <kristinguidi@gmail.com>
2017-11-09 10:22:00 -0800

    6.14 ActiveJob and Advanced Action Cable

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,6 +8,8 @@ class Comment < ActiveRecord::Base
   validates :product, presence: true
   validates :rating, numericality: { only_integer: true }
 
+  after_create_commit { CommentUpdateJob.perform_later(self, self.user) }
+  
   scope :rating_desc, -> { order(rating: :desc) }
 end
 
